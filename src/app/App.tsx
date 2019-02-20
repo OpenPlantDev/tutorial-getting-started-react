37b4ddbc985d857473f7aad02f660a164e2a1908
Dan Nichols <Dan.Nichols@bentley.com>
2019-02-20 11:26:10 -0600

    CORS test

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,42 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
 import { Hello } from "./components/Hello";
 
-export const App = () => {
+export class App extends React.Component {
+
+  render() {
    return (
-    <Hello compiler="Typescript" framework="React" bundler="Webpack" />
+     <div>
+      <Hello compiler="Typescript" framework="React" bundler="Webpack" />
+      <p>Check console for fetch results</p>
+     </div>
    );
+  }
+
+  fetchComponents()  {
+    let url = "http://localhost:4060/api/components";
+    console.log(`url: ${url}`);
+    return fetch(url).then((response) => {
+            if(response.ok) {
+                return response.json();
+            } else {
+                throw new Error("Error reading data");
+            }
+        }).then((result) => {
+            return result;
+        }).catch((err) => {
+            console.log(`In fetchComponents catch: ${err}`);
+            throw err;
+        });
+  }
+
+  async componentDidMount() {
+    console.log("In componentDidMount");
+
+    try {
+      const result = await this.fetchComponents();
+      console.log(result);
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
 };
